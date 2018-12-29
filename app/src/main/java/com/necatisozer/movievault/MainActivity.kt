4f425dc0ee7e2ad7021fb357b119f5eddb5b0610
necati <necatisozer@gmail.com>
2018-12-29 17:14:15 +0300

    Enable data binding

--- app/src/main/java/com/necatisozer/movievault/MainActivity.kt
+++ app/src/main/java/com/necatisozer/movievault/MainActivity.kt
@@ -2,13 +2,17 @@ package com.necatisozer.movievault
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.necatisozer.movievault.databinding.MainActivityBinding
 import com.necatisozer.movievault.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
