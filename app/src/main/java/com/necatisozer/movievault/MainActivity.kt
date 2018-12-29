2f127dfeccb80ad829b678df754de138c891ed86
necati <necatisozer@gmail.com>
2018-12-29 18:23:55 +0300

    Add architecture components

--- app/src/main/java/com/necatisozer/movievault/MainActivity.kt
+++ app/src/main/java/com/necatisozer/movievault/MainActivity.kt
@@ -3,8 +3,10 @@ package com.necatisozer.movievault
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.necatisozer.movievault.databinding.MainActivityBinding
 import com.necatisozer.movievault.ui.main.MainFragment
+import com.necatisozer.movievault.ui.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        binding.viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.executePendingBindings()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
