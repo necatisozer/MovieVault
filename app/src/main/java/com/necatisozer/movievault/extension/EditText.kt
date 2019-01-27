833abc92c23531d484dc7bd6ea326be62e47f897
necati <necatisozer@gmail.com>
2019-01-27 12:29:14 +0300

    Remove afterTextChanged extension

--- app/src/main/java/com/necatisozer/movievault/extension/EditText.kt
+++ app/src/main/java/com/necatisozer/movievault/extension/EditText.kt
@@ -1,19 +1,6 @@
 package com.necatisozer.movievault.extension
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.EditText
-
-inline fun EditText.afterTextChanged(crossinline listener: (String) -> Unit) {
-    addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-            listener(s.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-    })
-}
 
 fun EditText.clearBackground() {
     val paddingBottom = paddingBottom
