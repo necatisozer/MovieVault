54e131141f565bd2c5fb71bc35b12d1b8407d220
necati <necatisozer@gmail.com>
2020-02-08 06:54:28 +0300

    Refactor Gradle files

--- app/src/main/java/com/necatisozer/movievault/MainActivity.kt
+++ app/src/main/java/com/necatisozer/movievault/MainActivity.kt
@@ -2,6 +2,7 @@ package com.necatisozer.movievault
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commitNow
 import com.necatisozer.movievault.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+            supportFragmentManager.commitNow {
+                replace(R.id.container, MainFragment.newInstance())
+            }
         }
     }
 
--- app/src/main/java/com/necatisozer/movievault/ui/main/MainFragment.kt
+++ app/src/main/java/com/necatisozer/movievault/ui/main/MainFragment.kt
@@ -1,31 +1,20 @@
 package com.necatisozer.movievault.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.viewModels
 import com.necatisozer.movievault.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
